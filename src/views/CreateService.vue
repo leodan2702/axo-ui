d7eb3b30ca817ee230eef767525c02002e1b8a1b
leodan2702 <21690080@tecvalles.mx>
2025-09-09 15:54:58 -0600

    Add new views for functions, microservices, and services

---
+++ src/views/CreateService.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-main 
+    class="d-flex-column justify-center align-center pa-5"
+    style="min-height: 300px;"
+  >
+    <v-container>
+        <div class="d-flex align-center mb-4">
+          <h1>Create Service</h1>
+        </div>
+
+        <v-divider></v-divider>
+
+        <!-- Contenedor centrado del formulario -->
+        <v-sheet class="mt-5 d-flex justify-center" style="width: 100%;">
+          <FormCreateService />
+        </v-sheet>
+  
+    </v-container>                  
+  </v-main>
+</template>
+
+<script setup>
+import FormCreateService from '@/components/FormCreateService.vue';
+</script>
